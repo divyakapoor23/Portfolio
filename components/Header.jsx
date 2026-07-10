8364ca02c7a4456544e85489560965e0e9b4f0a8
Divya Kapoor <38455002+divyakapoor23@users.noreply.github.com>
2026-07-10 14:36:52 -0400

    updated header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,17 @@
 function Header(){
     return(
         <header>
-            {
-                /* navigation + logo name JSX goes here */
-            }
+            <div className = "logo">
+                <a href = "#hero">Divya Kapoor</a>
+            </div>
+            <nav>
+                <ul>
+                    <li> <a href = "#about"> About </a></li>
+                    <li> <a href = "#projects"> Projects </a></li>
+                    <li> <a href = "#contact"> Contact </a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
+export default Header
